f347e174c272b7155fc88d4810c2fab901deba47
KonstantinDenisov <omacs@mail.ru>
2023-01-30 19:38:15 +0300

    Create DynamicCellInitiation, CellPrefab

---
+++ Assets/Scripts/DynamicCellInitiation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCellInitiation : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private GameObject _cellPrefab;
+    private RectTransform _rectTransform;
+    private readonly List<GameObject> _cells;
+
+    #endregion
+
+
+    #region Unity LifeCycle
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    
+    private void HpChanged(int hp)
+    {
+        DestroyCurrentCells();
+        CreateNewCell(hp);
+    }
+
+    private void CreateNewCell(int hp)
+    {
+        for (int i = 0; i < hp; i++)
+        {
+            GameObject cell = Instantiate(_cellPrefab, _rectTransform);
+            _cells.Add(cell);
+        }
+    }
+
+    private void DestroyCurrentCells()
+    {
+        foreach (GameObject cell in _cells)
+        {
+            Destroy(cell);
+        }
+
+        _cells.Clear();
+    }
+
+    #endregion
+}
+  
